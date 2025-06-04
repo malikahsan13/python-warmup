464a4dbe7d878ac8ca203dcf14f3e7ff94449814
Malik Ahsan <malikahsan13@gmail.com>
2025-06-04 21:40:15 +0500

    added users endpoint which return array

--- fastapi-1/main.py
+++ fastapi-1/main.py
@@ -19,3 +19,12 @@ async def read_user_me():
 @app.get("/users/{user_id}")
 async def read_user(user_id: str):
     return {"user_id": user_id}
+
+@app.get("/users")
+async def read_users():
+    return ["Rick", "Morty"]
+
+
+@app.get("/users")
+async def read_users2():
+    return ["Bean", "Elfo"]
