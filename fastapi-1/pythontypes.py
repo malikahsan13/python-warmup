e53a0e9a88cc6276743fe105f2a1055c02e6b023
Malik Ahsan <malikahsan13@gmail.com>
2025-06-05 20:08:05 +0500

    list type testing

--- fastapi-1/pythontypes.py
+++ fastapi-1/pythontypes.py
@@ -1,4 +1,4 @@
-def add(firstname : str | list, lastname: str = None):
+def add(firstname : str | list[int], lastname: str = None):
     return firstname+" "+lastname
 
 
@@ -7,3 +7,10 @@ lname = "Gates"
 
 name = add(fname)
 print(name)
+
+
+def showNumbers(numberList : list[int, int, bool]):
+    for i in numserList:
+        print(i)
+
+showNumbers([True, True, 3])
