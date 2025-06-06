a9280610aa7bdfbc98c47a822f7c58a7928a48a5
Malik Ahsan <malikahsan13@gmail.com>
2025-06-06 23:54:17 +0500

    added method for updating item

--- fastapi-1/main.py
+++ fastapi-1/main.py
@@ -30,6 +30,10 @@ async def create_item(item: Item):
         item_dict.update({"price_with_tax": price_with_tax})
     return item_dict
 
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    return {"item_id": item_id, **item.dict()}
+
 @app.get("/users/me")
 async def read_user_me():
     return {"user_id": "the current user"}
