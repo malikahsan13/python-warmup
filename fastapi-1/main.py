6aa4144a4446627a1105643a0cc765964cc9cb72
Malik Ahsan <malikahsan13@gmail.com>
2025-06-06 23:50:47 +0500

    query params addition

--- fastapi-1/main.py
+++ fastapi-1/main.py
@@ -28,3 +28,10 @@ async def read_users():
 @app.get("/users")
 async def read_users2():
     return ["Bean", "Elfo"]
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]    
