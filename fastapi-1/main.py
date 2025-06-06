2b287f3ce48c5f47f0aaa310f7d0a2bebc92df34
Malik Ahsan <malikahsan13@gmail.com>
2025-06-06 23:53:25 +0500

    added additional logic for creating item

--- fastapi-1/main.py
+++ fastapi-1/main.py
@@ -18,9 +18,17 @@ async def read_item(item_id):
     return {"item_id": item_id}
 
 
+# @app.post("/items/")
+# async def create_item(item: Item):
+#     return item
+
 @app.post("/items/")
 async def create_item(item: Item):
-    return item
+    item_dict = item.dict()
+    if item.tax is not None:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
 
 @app.get("/users/me")
 async def read_user_me():
