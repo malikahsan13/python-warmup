cfe4b3379f09e0140b8aa806e71af88bf4e4ae4b
Malik Ahsan <malikahsan13@gmail.com>
2025-06-08 23:59:53 +0500

    added line for testing

--- fastapi-1/main.py
+++ fastapi-1/main.py
@@ -72,7 +72,7 @@ async def read_items(
         str | None, Query(min_length=3, max_length=50, pattern="^fixedquery$")
     ] = None,
 ):
-    # results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
     # if q:
     #     results.update({"q": q})
     return results
