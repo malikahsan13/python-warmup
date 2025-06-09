c0b7d56911f8d20e47df0f5b24ffb13960775420
Malik Ahsan <malikahsan13@gmail.com>
2025-06-09 23:51:16 +0500

    add number validation ge

--- fastapi-1/main.py
+++ fastapi-1/main.py
@@ -18,10 +18,10 @@ async def root():
 # async def read_item(item_id):
 #     return {"item_id": item_id}
 
+
 @app.get("/items/{item_id}")
 async def read_items(
-    item_id: Annotated[int, Path(title="The ID of the item to get")],
-    q: Annotated[str | None, Query(alias="item-query")] = None,
+    item_id: Annotated[int, Path(title="The ID of the item to get", ge=1)], q: str
 ):
     results = {"item_id": item_id}
     if q:
