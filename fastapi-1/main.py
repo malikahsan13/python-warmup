7f7b46fee4b90f5b7c8c6a04cd704bc7ead96fce
Malik Ahsan <malikahsan13@gmail.com>
2025-06-10 18:36:03 +0500

    added template in fast api and served a html page

--- fastapi-1/main.py
+++ fastapi-1/main.py
@@ -1,15 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from pydantic import BaseModel
 from typing import Union
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from pymongo import MongoClient
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+conn = MongoClient()
+
+@app.get("/", response_class=HTMLResponse)
+async def getIndex(request: Request):
+    return templates.TemplateResponse("index.html", { "request": request })
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str | None = None):
