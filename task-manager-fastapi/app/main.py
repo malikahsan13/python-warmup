e463a093db0f3e07165cf2c6fff531353f24eb72
Malik Ahsan <malikahsan13@gmail.com>
2025-06-11 17:17:38 +0500

    added swagger ui parameter

--- task-manager-fastapi/app/main.py
+++ task-manager-fastapi/app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
 from app.routes import task_routes
 
-app = FastAPI()
+app = FastAPI(swagger_ui_parameters={"syntaxHighlight": False})
 app.include_router(task_routes.router, prefix="/tasks", tags=["Tasks"])
