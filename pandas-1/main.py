d938e29ae4bdaaa65f62d314acdaf708101b2944
Malik Ahsan <malikahsan13@gmail.com>
2025-06-12 23:44:02 +0500

    DataFrame (2D Data)

--- pandas-1/main.py
+++ pandas-1/main.py
@@ -2,3 +2,11 @@ import pandas as pd  # Common convention
 
 data = pd.Series([10, 20, 30, 40])
 print(data)
+
+data = {
+    "Name": ["Alice", "Bob", "Charlie"],
+    "Age": [25, 30, 35],
+    "City": ["New York", "London", "Tokyo"]
+}
+df = pd.DataFrame(data)
+print(df)
