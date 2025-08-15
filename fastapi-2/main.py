fa6a3db1f48accaed62997b689a4715a275962cb
Malik Ahsan <malikahsan13@gmail.com>
2025-08-15 22:45:37 +0500

    testing query params

--- fastapi-2/main.py
+++ fastapi-2/main.py
@@ -7,6 +7,10 @@ def showHelloWorld():
     return {"hello":"world"}
 
 
-@app.get("get_items/{item_id}")
+@app.get("/get_items/{item_id}")
 def get_item(item_id: int):
     return {"item_id": item_id}
+
+@app.get("/get_items_query")
+def get_item_query(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
