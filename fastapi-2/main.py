4f90293346cd0d3c473fe3f91d5f6d7ad1687e39
Malik Ahsan <malikahsan13@gmail.com>
2025-08-19 23:22:25 +0500

    added return statement

--- fastapi-2/main.py
+++ fastapi-2/main.py
@@ -5,3 +5,6 @@ app = FastAPI()
 @app.post("/submit-form/")
 def submit_form(username: str = Form(...), password: str = Form(...)):
     print("username", username)
+    print("password", password)
+    
+    return {"msg":"Form submitted successfully", "username": username}
